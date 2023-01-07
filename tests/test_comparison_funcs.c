63e3dccbd7d6ae7e336da71bd33a9087ee1abe75
Nick <nfeezor@gmail.com>
2023-01-07 14:54:05 -0600

    Removed old TestSuite copy - now I'm just pointing it to the source project file for TestSuite (as I may need more changes to TestSuite)

--- tests/test_comparison_funcs.c
+++ tests/test_comparison_funcs.c
@@ -1,32 +1,29 @@
-#include "../test_suite/test_suite.h"
+#include "D:\Projects\c-testsuite\test_suite\test_suite.c"
 #include "../c_version/letter_score.h"
 
-
-
-void test_letter_score(TestSuite *ts)
+void test_letter_score()
 {
-    double score;
+
     // Exact equal distribution - should be 1
+    ASSERT_EQUAL_DOUBLE(letter_score("HELLO", "HELLO"), 1.0);
 
-    score = letter_score("abbbcdef", "bbbcdfea");
-    assert_equal_double(ts, "letter score EQUAL DIST", score, 1);
+    // // Exact different distribution - should be 0
+    ASSERT_EQUAL_DOUBLE(letter_score("ABCDE", "ZYXWV"), 0.0);
 
-    // Exact different distribution - should b 0
-    score = letter_score("abcde", "zyxwv");
-    assert_equal_double(ts, "letter score NONEQUAL DIST", score, 0);
+    // // Should be 50% similar
+    ASSERT_EQUAL_DOUBLE(letter_score("ABCD", "ABZZ"), 0.5);
 
-    // Should be 50% similar
-    score = letter_score("abcd", "abzz");
-    assert_equal_double(ts, "letter score HALF EQUAL", score, 0.5);
+    // Reversibility
+    ASSERT_EQUAL_DOUBLE(
+        letter_score("THIS STRING", "THAT STRING"),
+        letter_score("THAT STRING", "THIS STRING")
+    );
 }
 
 
 int main()
 {
-    TestSuite *ts = TestSuite_init();
-
-    test_letter_score(ts);
-
-    TestSuite_print(ts);
-    TestSuite_deconstruct(ts);
+    TEST_START;
+    ADD_CASE(test_letter_score, "letter score");
+    TEST_END;
 }
