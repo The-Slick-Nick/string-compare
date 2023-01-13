2deb325da95c8636f1c83de6aa64b7bf7932c021
The-Slick-Nick <=>
2023-01-13 13:47:01 -0600

    Adjusted setup.py for new naming conventions

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ import sys
 import os
 from setuptools import setup, Extension
 
+
 def main():
     setup(
         name="strcompare",
         version="1.0.0",
         description="Methods to assess string similarity.",
-        ext_modules=[Extension("strcompare", ["c_version\\string_compare_module.c"])]
+        ext_modules=[
+            Extension("strcompare", ["strcompare\\strcompare_module.c"])
+        ]
     )
+
 
 if __name__ == "__main__":
     main()
