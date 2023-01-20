7bed4dcc6638b18065466be181b044c26a20916e
Nick <nfeezor@gmail.com>
2023-01-19 20:29:42 -0600

    Updated setup/packaging stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 import sys
 import os
-from setuptools import setup, Extension
+import sysconfig
+from setuptools import setup, Extension, find_packages
 
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text()
 
 def main():
     setup(
         name="strcompare",
         version="1.2.1",
         description="Methods to assess string similarity.",
-        ext_modules=[Extension("strcompare", ['src\\strcompare_module.c'])]
+        ext_modules=[Extension(
+            name="strcompare",
+            sources=['src\\strcompare_module.c'],
+        )],
+        author="Slick Nick",
+        author_email="nfeezor@gmail.com",
+        url="https://github.com/The-Slick-Nick/string-compare",
+        long_description=long_description,
+        long_description_content_type='text/markdown'
     )
 
 
